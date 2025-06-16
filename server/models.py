665cbfd057953e6e16a13e92321d6120bd50d7a6
theMungai <mungaithesimon@gmail.com>
2025-06-16 11:25:24 +0300

    Uses Flask-SQLAlchemy to configure a database connection

--- server/models.py
+++ server/models.py
@@ -14,5 +14,6 @@ class Pet(db.Model):
     __tablename__ = 'pets'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    species = db.Column(db.String)
+    username = db.Column(db.String(80), unique=True,nullable=False, index=True)
+    email = db.Column(db.String(120), unique=True)
+    verified = db.Column(db.Boolean, default=False)
